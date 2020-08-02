fea73a4b83d8532b23c96ad144ba75bcfb091130
Ayush Shrivastava <61715493+ayushshrivastava28@users.noreply.github.com>
2020-08-02 19:26:01 +0530

    Update readData.jsx

--- src/readData.jsx
+++ src/readData.jsx
@@ -2,19 +2,16 @@ const readData=[
     {
         id: 1,
         title: "HAIR CARE 101",
-        content: "with Dr Shobita Anand",
         image: "https://www.roughguides.com/wp-content/uploads/2016/02/matterhorn-shutterstock_1118486243.jpg"
       },
       {
         id: 2,
         title: "HOW TO USE NIACIN",
-        content: "Follow up on your regime",
         image: "https://www.nationalgeographic.com/content/dam/travel/rights-exempt/2018-travel-photographer-of-the-year/magical-mountains/mountain-range-appenzell-switzerland.jpg"
       },
       {
         id: 3,
         title: "EFFECTS OF MINOXIDIL",
-        content: "Use your shampoo",
         image: "https://i.pinimg.com/originals/71/d7/c0/71d7c0d0eb06eabd2be186ee79c2ecb2.jpg"
       },
       
