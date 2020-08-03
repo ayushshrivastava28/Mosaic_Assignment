37b09c4be6f8dcea9d64c07977024ca47028370e
Ayush Shrivastava <61715493+ayushshrivastava28@users.noreply.github.com>
2020-08-03 15:50:03 +0530

    Update Cards.jsx

--- src/Cards.jsx
+++ src/Cards.jsx
@@ -4,11 +4,14 @@ function Card(props){
       <>
     <div className="cards">
       <div className="card">
-        {props.image ? (<img style={{ width: "200px" }} src={props.image} alt="ReadDataPic" className="card_img"/>) : ("")}
         <div className="card_info" >
-          <h3 className="card_category">{props.title}</h3>
+          <h6 className="card_heading">{props.id}</h6>
+          {props.image ? (<img  src={props.image} alt="ReadDataPic" className="card_img"/>) : ("")}  
+          <h4 className="card_category">{props.title}</h4>
           <h5 className="card_title">{props.content}</h5>
           <h6 className="card_title" style={{color:"magenta"}}>{props.time}</h6>
+          <a href="#" className="card_link">{props.link}</a>
+          
         </div>
       </div>
     </div>
